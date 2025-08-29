7a4f462f0dc12ddb77428e3340f54f39efde3573
JerryLing <lingxinghao140@gmail.com>
2025-08-29 14:35:17 +0800

    Filters Data updated.

--- SmilE/Optics_exp/Database_of_filters/importSpectrum.py
+++ SmilE/Optics_exp/Database_of_filters/importSpectrum.py
@@ -19,6 +19,26 @@ def read_filter_csv(file_path, wavelength_col=0, transmission_col=1, skip_header
                 continue  # 遇到空行/非数字行则跳过
     return np.array(wavelengths), np.array(transmissions)
 
+def read_filter_txt(file_path, wavelength_col=0, transmission_col=1, skip_header=True):
+    """Read from csv file the specturm into 2 np arrays.
+       Transmission values will be multiplied by 100.
+    """
+    wavelengths, transmissions = [], []
+    with open(file_path, 'r', encoding='utf-8') as f:
+        reader = csv.reader(f, delimiter='\t')
+        if skip_header:
+            next(reader, None)  
+        for row in reader:
+            try:
+                wl = float(row[wavelength_col])
+                tr = float(row[transmission_col]) * 100.0  
+                wavelengths.append(wl)
+                transmissions.append(tr)
+            except ValueError:
+                continue  
+    return np.array(wavelengths), np.array(transmissions)
+
+
 def resample_spectrum(wavelengths, transmissions, wl_min=300, wl_max=900, step=0.5, kind="linear"):
     """Interpolate linearly so that wavelength form is the same. Default range is 300-900@0.5nm/step."""
     if wl_min is None:
